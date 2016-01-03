5a45036a2875a00237d3b5ab33fb324c7fafcbb8
KotBarsic <KotBarsic@8155e7df-53bb-40e7-a330-803f01620f43>
2016-01-03 07:39:59 +0000

    Переопределил SimpleCursorAdapter что бы форматировать деньги
    git-svn-id: https://subversion.assembla.com/svn/university32bgtu@263 8155e7df-53bb-40e7-a330-803f01620f43

--- OurBudget/src/com/BloodliviyKot/OurBudget/Accounts.java
+++ OurBudget/src/com/BloodliviyKot/OurBudget/Accounts.java
@@ -1,14 +1,17 @@
 package com.BloodliviyKot.OurBudget;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 
 public class Accounts
@@ -33,7 +36,7 @@ MySQLiteOpenHelper.debugDeleteDB(getApplicationContext());
 
     //Cursor обязательно должен содержать _id иначе SimpleCursorAdapter не заработает
     Cursor c = db.rawQuery("SELECT account._id, account.name, account.balance FROM account;", null);
-    ListAdapter list_adapter = new SimpleCursorAdapter(this, R.layout.accounts_item, c,
+    ListAdapter list_adapter = new account_SimpleCursorAdapter(this, R.layout.accounts_item, c,
       new String[]{"name", "balance"},
       new int[]{R.id.accounts_item_name, R.id.accounts_item_balance});
     list_accounts.setAdapter(list_adapter);
@@ -63,6 +66,37 @@ MySQLiteOpenHelper.debugDeleteDB(getApplicationContext());
   }
 
 
-
-
 }
+
+//Переопределим SimpleCursorAdapter что бы форматировать данные из базы нужным образом
+class account_SimpleCursorAdapter
+  extends SimpleCursorAdapter
+{
+  public account_SimpleCursorAdapter(Context _context, int _layout, Cursor _c,
+                                     String[] _from, int[] _to)
+  {
+    super(_context, _layout, _c, _from, _to);
+  }
+  @Override
+  public void bindView(View _view, Context _context, Cursor _cursor)
+  {
+    String name = _cursor.getString(_cursor.getColumnIndex("name"));
+    double balance = Double.parseDouble(_cursor.getString(_cursor.getColumnIndex("balance")));
+    Integer balance_currency = (int)balance;
+    Integer balance_currency_small = (int)((balance - balance_currency) * 100);
+    TextView nameTV = (TextView)_view.findViewById(R.id.accounts_item_name);
+    TextView balanceTV = (TextView)_view.findViewById(R.id.accounts_item_balance);
+    nameTV.setText(name);
+    balanceTV.setText(balance_currency.toString() + _context.getString(R.string.common_currency) +
+      balance_currency_small.toString() + _context.getString(R.string.common_currency_small));
+  }
+}
+
+
+
+
+
+
+
+
+
