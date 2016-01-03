260b1f64f201dd0bd65d6b9624ffe1a2da9e0404
KotBarsic <KotBarsic@8155e7df-53bb-40e7-a330-803f01620f43>
2016-01-03 23:56:13 +0000

    Реализовал добавление дистрибутивных данных в БД через xml/distrib_db.xml
    git-svn-id: https://subversion.assembla.com/svn/university32bgtu@267 8155e7df-53bb-40e7-a330-803f01620f43

--- OurBudget/res/raw/v_001_tables.ddl
+++ OurBudget/res/raw/v_001_tables.ddl
@@ -5,3 +5,23 @@ CREATE TABLE account
   name                 TEXT NOT NULL,
   balance              DECIMAL NOT NULL
 );
+
+CREATE TABLE type_movement
+(
+  _id                  INTEGER PRIMARY KEY,
+  _paren_type_movement INTEGER,
+  name                 TEXT NOT NULL,
+  trend                INTEGER NOT NULL,
+  _id_account          INTEGER NOT NULL,
+  FOREIGN KEY (_paren_type_movement) REFERENCES type_movement (_id),
+  FOREIGN KEY (_id_account) REFERENCES account (_id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE movement
+(
+  _id                  INTEGER PRIMARY KEY,
+  _id_account          INTEGER NOT NULL,
+  _id_type_movement    INTEGER NOT NULL,
+  id_union             INTEGER NOT NULL,
+  FOREIGN KEY (_id_type_movement) REFERENCES type_movement (_id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
--- OurBudget/src/com/BloodliviyKot/OurBudget/MySQLiteOpenHelper.java
+++ OurBudget/src/com/BloodliviyKot/OurBudget/MySQLiteOpenHelper.java
@@ -1,14 +1,19 @@
 package com.BloodliviyKot.OurBudget;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
 import com.BloodliviyKot.tools.SQLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Stack;
 
 public class MySQLiteOpenHelper
   extends SQLiteOpenHelper
@@ -16,12 +21,13 @@ public class MySQLiteOpenHelper
   public static final int VersionDB = 1; //Версия БД
   public static final String DBName = "OurBudget.db";
   private SQLReader sql_reader;
-
-  public MySQLiteOpenHelper(Context context)
+  private Context context;
+  public MySQLiteOpenHelper(Context _context)
   {
     //Здесь создается или открывается БД
-    super(context, DBName, null, VersionDB);
-    sql_reader = new SQLReader(context.getResources());
+    super(_context, DBName, null, VersionDB);
+    context = _context;
+    sql_reader = new SQLReader(_context.getResources());
   }
   //Создаем таблицы базы
   @Override
@@ -41,24 +47,72 @@ public class MySQLiteOpenHelper
     fillDistr(db);
   }
   //Дистрибутивное наполнение таблиц базы
+  private static final String SKIP_TAG = "distrib";
+  private static final String USE_PARENT_ID = "ИД_родителя";
   private void fillDistr(SQLiteDatabase db)
   {
-    //Заполним дистрибутивное содержание списка счетов
     try
     {
-      /*
+      //Заполним дистрибутивное содержание данных в БД
+      Stack<Long> stack_id = new Stack<Long>();
       ContentValues values = new ContentValues();
-      values.put("name", "Имя");
-      values.put("balance", "435.76");
-      long _id = db.insert("account", null, values);
-      */
-      List<String> queris = sql_reader.getQueris(R.raw.distr_accounts);
-      for(String query : queris)
-        db.execSQL(query);
+      long _id = 0;
+      XmlPullParser xpp = context.getResources().getXml(R.xml.distrib_db);
+      while (xpp.getEventType() != XmlPullParser.END_DOCUMENT)
+      {
+        switch (xpp.getEventType())
+        {
+          // начало документа
+          case XmlPullParser.START_DOCUMENT:
+            break;
+          // начало тэга
+          case XmlPullParser.START_TAG:
+            stack_id.push(_id);
+            String  table_name = xpp.getName();
+            if(table_name.compareTo(SKIP_TAG) == 0 )
+              break;
+            values.clear();
+            for(int i = 0; i < xpp.getAttributeCount(); i++)
+            {
+              String attr_name = xpp.getAttributeName(i);
+              String attr_value = xpp.getAttributeValue(i);
+              if(attr_value.compareTo(USE_PARENT_ID) == 0)
+              {
+                if(_id == 0)
+                  continue; //Отсекаем попытку вставить элемент верхнего уровня как дочерний
+                attr_value = Long.toString(_id);
+              }
+              values.put(attr_name, attr_value);
+            }
+            _id = db.insert(table_name, null, values);
+            break;
+          // конец тэга
+          case XmlPullParser.END_TAG:
+            _id = stack_id.pop();
+            break;
+          // содержимое тэга
+          case XmlPullParser.TEXT:
+            //xpp.getText();
+            break;
+
+          default:
+            break;
+        }
+        // следующий элемент
+        xpp.next();
+      }
     }
     catch(SQLException e)
     {
       String err = e.getMessage();
+    }
+    catch(XmlPullParserException e)
+    {
+      e.printStackTrace();
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
     }
   }
 
