97c23db968e0dc4f3b4de284c2819d5a21167516
KotBarsic <KotBarsic@8155e7df-53bb-40e7-a330-803f01620f43>
2016-02-04 12:55:27 +0000

    Доделал диалог ввода даты времени покупки, почти.
    git-svn-id: https://subversion.assembla.com/svn/university32bgtu@378 8155e7df-53bb-40e7-a330-803f01620f43

--- OurBudget/src/com/BloodliviyKot/OurBudget/Dialogs/PurchaseDateTimeDialog.java
+++ OurBudget/src/com/BloodliviyKot/OurBudget/Dialogs/PurchaseDateTimeDialog.java
@@ -1,7 +1,9 @@
 package com.BloodliviyKot.OurBudget.Dialogs;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,17 +13,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import com.BloodliviyKot.OurBudget.R;
 import com.BloodliviyKot.tools.DataBase.MySQLiteOpenHelper;
 
+import java.util.Date;
 import java.text.SimpleDateFormat;
 
 @SuppressLint("ValidFragment")
 public class PurchaseDateTimeDialog
   extends DialogFragment
-  implements View.OnClickListener
+  implements View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
+  public static final int YEAR_CORRECTOR = 1900;
   private MySQLiteOpenHelper oh;
   private SQLiteDatabase db;
 
@@ -51,30 +57,36 @@ public class PurchaseDateTimeDialog
 
     oh = new MySQLiteOpenHelper(v.getContext());
     db = oh.getWritableDatabase();
-
-    String string_date_time[] = new String[2];
-    getStringDateTime(date_time, string_date_time);
-    et_date.setText(string_date_time[0]);
-    et_time.setText(string_date_time[1]);
-
+    updateView();
     et_date.setOnClickListener(this);
     et_time.setOnClickListener(this);
     button_save.setOnClickListener(this);
 
     return v;
   }
+  private void updateView()
+  {
+    String string_date_time[] = new String[2];
+    getStringDateTime(date_time, string_date_time);
+    et_date.setText(string_date_time[0]);
+    et_time.setText(string_date_time[1]);
+  }
   @Override
   public void onClick(View v)
   {
     if(v == et_date)
     {
-
-
+      Date date = new Date(date_time);
+      DatePickerDialog dpd = new DatePickerDialog(v.getContext(), this, date.getYear() + YEAR_CORRECTOR,
+                                                  date.getMonth(), date.getDate());
+      dpd.show();
     }
     else if(v == et_time)
     {
-
-
+      Date date = new Date(date_time);
+      TimePickerDialog tpd = new TimePickerDialog(v.getContext(), this, date.getHours(),
+                                                  date.getMinutes(), true);
+      tpd.show();
     }
     else if(v == button_save)
     {
@@ -82,6 +94,26 @@ public class PurchaseDateTimeDialog
       dismiss();
     }
   }
+  @Override
+  public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+  {
+    Date date = new Date(date_time);
+    date.setYear(year - YEAR_CORRECTOR);
+    date.setMonth(monthOfYear);
+    date.setDate(dayOfMonth);
+    date_time = date.getTime();
+    updateView();
+  }
+  @Override
+  public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+  {
+    Date date = new Date(date_time);
+    date.setHours(hourOfDay);
+    date.setMinutes(minute);
+    date_time = date.getTime();
+    updateView();
+  }
+
   @Override
   public void onCancel(DialogInterface dialog)
   {
@@ -102,7 +134,7 @@ public class PurchaseDateTimeDialog
   public static void getStringDateTime(long date_time, String result_date_time[])
   {
     SimpleDateFormat date_format = new SimpleDateFormat("dd.MM.yyyy");
-    SimpleDateFormat time_format = new SimpleDateFormat("hh:mm");
+    SimpleDateFormat time_format = new SimpleDateFormat("HH:mm"); //HH 0-23  hh 0-12am-pm
     result_date_time[0] = date_format.format(date_time);
     result_date_time[1] = time_format.format(date_time);
   }
