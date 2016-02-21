3b96a27657d0280777b7a62bb83bf105e4842fe8
KotBarsic <KotBarsic@8155e7df-53bb-40e7-a330-803f01620f43>
2016-02-21 16:32:35 +0000

    Чисто для примера Сервис выводит и озвучивает уведомление, при нажатии на которое вызывается активность.
    git-svn-id: https://subversion.assembla.com/svn/university32bgtu@426 8155e7df-53bb-40e7-a330-803f01620f43

--- OurBudget/src/com/BloodliviyKot/synchronization/ServiceSynchronization.java
+++ OurBudget/src/com/BloodliviyKot/synchronization/ServiceSynchronization.java
@@ -1,11 +1,18 @@
 package com.BloodliviyKot.synchronization;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+import com.BloodliviyKot.OurBudget.R;
+import com.BloodliviyKot.OurBudget.WPurchases;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +25,7 @@ public class ServiceSynchronization
   public void onCreate()
   {
     super.onCreate();
+    nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     Log.d(LOG_TAG, "onCreate");
   }
 
@@ -50,23 +58,46 @@ public class ServiceSynchronization
         {
           try
           {
-            TimeUnit.SECONDS.sleep(7);
-
+            TimeUnit.SECONDS.sleep(17);
             handler.post(new Runnable()
             {
               public void run()
               {
                 Toast.makeText(getApplicationContext(), "msg", Toast.LENGTH_LONG).show();
+                sendNotif();
               }
             });
           } catch(InterruptedException e)
           {
             e.printStackTrace();
           }
-          //stopSelf();
+          stopSelf();
+          break;
         }
       }
     }).start();
+  }
+
+  NotificationManager nm;
+  void sendNotif()
+  {
+    // 1-я часть
+    Notification notif = new Notification(R.drawable.ic_launcher, "Text in status bar", System.currentTimeMillis());
+    // 3-я часть
+    Intent intent = new Intent(this, WPurchases.class);
+    //intent.putExtra(Purchase.FILE_NAME, "somefile");
+    PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+    // 2-я часть
+    notif.setLatestEventInfo(this, "Notification's title", "Notification's text", pIntent);
+
+    Uri ringURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+    notif.sound = ringURI;
+
+    // ставим флаг, чтобы уведомление пропало после нажатия
+    notif.flags |= Notification.FLAG_AUTO_CANCEL;
+
+    // отправляем
+    nm.notify(1, notif);
   }
 
 }
